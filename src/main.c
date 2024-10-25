6e515aa04048c8452ff5fa0a797fb29c7719265b
PabloAvelar <pabloarmenta84@gmail.com>
2024-10-25 00:10:15 -0600

    semaforos con 5 hilos para un arreglo

--- src/main.c
+++ src/main.c
@@ -4,10 +4,15 @@
 
 #define MAX_SEM_COUNT 10
 #define THREADCOUNT 5
+#define BUFFER_SIZE 10
 
 HANDLE s;
+
 DWORD WINAPI tarea(LPVOID param);
-int shared_resource = 0;
+
+int shared_resource[BUFFER_SIZE];
+int resource_counter = 0;
+
 int main(void) {
     HANDLE threads[THREADCOUNT];
     int threads_ids[MAX_SEM_COUNT];
@@ -16,10 +21,10 @@ int main(void) {
     // Se crea un semáforo con un conteo máximo de MAX_SEM_COUNT
     s = CreateSemaphore(
         NULL,
-        MAX_SEM_COUNT,
-        MAX_SEM_COUNT,
+        1,
+        1,
         NULL
-        );
+    );
 
     if (!s) {
         printf("CreateSemaphore error: %d\n", GetLastError());
@@ -36,7 +41,7 @@ int main(void) {
             &threads_ids[i],
             0,
             &ThreadID
-            );
+        );
 
         if (!threads[i]) {
             printf("CreateThread error: %d\n", GetLastError());
@@ -45,54 +50,63 @@ int main(void) {
     }
 
     WaitForMultipleObjects(THREADCOUNT, threads, TRUE, INFINITE);
-    for(int i = 0; i < THREADCOUNT; i++) {
+    for (int i = 0; i < THREADCOUNT; i++) {
         CloseHandle(threads[i]);
     }
 
     CloseHandle(s);
 
     printf("Todos los hilos han terminado.\a\n");
+    printf("Resultado final: [");
+    for(int i = 0; i < BUFFER_SIZE; i++) {
+        printf("%d, ", shared_resource[i]);
+    }
+    printf("]\n");
     system("pause");
     return 0;
 }
 
 DWORD WINAPI tarea(LPVOID param) {
     int thread_id = *(int *) param; // Convierte el parámetro recibido
-    printf("Soy un hilo ejecutando la tarea: %d\n", thread_id);
+    printf("\n[Thread %d] Iniciando la tarea\n", thread_id);
 
-    DWORD dwWaitResult;
-    BOOL bContinue = TRUE;
+    DWORD dwWaitResult = WaitForSingleObject(
+        s,
+        INFINITE
+    );
 
-    while(bContinue) {
+    if (dwWaitResult == WAIT_OBJECT_0) {
         // Intentando entrar
-        dwWaitResult = WaitForSingleObject(
-            s,
-            5000
-            );
-        switch (dwWaitResult) {
-            // Si se lanzó un Signal
-            case WAIT_OBJECT_0:
-                printf("Thread %d: wait succeded\n", GetCurrentThreadId());
-                bContinue=FALSE;
-                // Simulando tiempo de tarea
-                Sleep(3000);
 
-            // Soltado el semáforo cuando termina la tarea
-            if (!ReleaseSemaphore(
-                s,
-                1, // Incremento del contador
-                NULL
-                )) {
-                printf("ReleaseSemaphore error: %d\n", GetLastError());
-            }
-            break;
 
-            // Si el semáforo no recibió un Signal, entonces ocurre un tiempo de espera
-            case WAIT_TIMEOUT:
-                printf("Thread %d: wait timed out\n", GetCurrentThreadId());
-            break;
+        system("cls");
+
+        // Mostrando el arreglo
+        printf("\t[");
+        for(int i = 0; i < BUFFER_SIZE; i++) {
+            printf("%d, ", shared_resource[i]);
         }
+        printf("]\n");
+
+        printf("Thread %d: su espera tuvo exito\n", GetCurrentThreadId());
+
+        // Sección crítica
+        shared_resource[resource_counter] = ++resource_counter;
+        printf("[Thread %d] Recurso compartido modificado %d\n", thread_id);
+        Sleep(2000);
+
+        // Liberar el semáforo
+        if (!ReleaseSemaphore(s, 1, NULL)) {
+            printf("[Thread %d] Error al liberar el semáforo: %d\n", thread_id, GetLastError());
+        } else {
+            printf("[Thread %d] Semaforo liberado\n", thread_id);
+        }
+    } else {
+        printf("[Thread %d] Error al intentar adquirir el semáforo\n", thread_id);
     }
 
-    return TRUE;
+    printf("[Thread %d] Tarea finalizada\n", thread_id);
+
+
+    return 0;
 }
